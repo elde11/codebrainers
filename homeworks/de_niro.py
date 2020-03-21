891ddb853a441fd37380537773729bb40ed001eb
elde11 <32717621+elde11@users.noreply.github.com>
2020-03-21 15:04:04 +0100

    Update de_niro.py

--- homeworks/de_niro.py
+++ homeworks/de_niro.py
@@ -25,8 +25,7 @@ with open("przykladowy.csv", "r") as f:
             }
         )
 
-lista = [86,17,73,40,98,88,97,41,99,47,67,93,97,75,90,89,60,98,65,10,80,78,96,64,47,88,29,96,76,69,65,71,87,67,75,78,96,39,80,86,74,38,80,85,87,72,68,38,69,43,43,84,41,73,33,48,27,4,35,38,4,46,13,54,21
-,76,19,51,46,72,10,50,25,7,70,92,51,29,46,7,29,11,9,60,26,61,11]
+
 suma = 0
 liczba_elementów = 0
 
@@ -34,7 +33,7 @@ for i in data:
     suma += i
     liczba_elementów += 1
 
-print(f"Średnia Arytmetyczna ocen filmów RdN to:",suma/liczba_elementów)
+print(f"Średnia Arytmetyczna ocen filmów RdN to:", suma/liczba_elementów)
 
 
 
